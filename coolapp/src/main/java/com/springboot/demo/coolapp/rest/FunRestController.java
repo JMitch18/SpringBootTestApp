1674290ae356289c51be2e571620e572e83fe58a
JMITCH18 <ronmitchell1@yahoo.com>
2019-03-03 20:05:58 -0600

    Added functionality to home controller

--- coolapp/src/main/java/com/springboot/demo/coolapp/rest/FunRestController.java
+++ coolapp/src/main/java/com/springboot/demo/coolapp/rest/FunRestController.java
@@ -1,5 +1,17 @@
 package com.springboot.demo.coolapp.rest;
 
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
 
+	// enable home route to return hello world
+	
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hello World! Time on the server is " + LocalDateTime.now();	}
+	
 }
